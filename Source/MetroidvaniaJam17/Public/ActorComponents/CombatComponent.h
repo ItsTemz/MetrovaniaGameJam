31c1517940a94bdad1363b1bbe5f566d4a5dfd66
GustavoHorn <109387103+GustavoHorn@users.noreply.github.com>
2022-09-11 21:22:04 +0100

    Added rails

--- Source/MetroidvaniaJam17/Public/ActorComponents/CombatComponent.h
+++ Source/MetroidvaniaJam17/Public/ActorComponents/CombatComponent.h
@@ -48,6 +48,8 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
 	UFUNCTION(BlueprintCallable, Category = "Combat")
 	void SetActionMode(const EActionMode InActionMode);
 
