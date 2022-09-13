f2148d37ef40015a6103d3a5d6676b4e7994b4e9
ItsTemz <34264834+ItsTemz@users.noreply.github.com>
2022-09-13 21:56:51 +0200

    Added Boss

---
+++ Source/MetroidvaniaJam17/Public/AI/AIAnimInstance.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AIAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METROIDVANIAJAM17_API UAIAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+	
+	virtual void NativeInitializeAnimation() override;
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta =(AllowPrivateAccess = "true"))
+	class ABossAI* BossAI;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta =(AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta =(AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta =(AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+};
